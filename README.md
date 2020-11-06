788d4fa5491b4190f8890e9338105f00b876515a
H. Kamran <hkamran@unisontech.org>
2020-11-06 15:36:32 -0800

    Update gitignore, add workflow, add script, prepare README for data source URL with Jinaj2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# DataSource branch
+# DataSource Branch of SupportDocs
+## This is where SupportDocs gets its data!
 
-This is where SupportDocs gets its data!
+Data Source URL: <a id="datasource_url" href="{{ datasource_url }}">{{ datasource_url }}</a>
---
+++ _scripts/datasource_generator.py
@@ -0,0 +1,80 @@
+"""
+Data Source Generator
+Contributors:
+    :: H. Kamran [@hkamran80] (author)
+Version: 1.0.0
+Last Updated: 2020-10-09, @hkamran80
+"""
+
+import frontmatter
+import jinja2
+import codecs
+import json
+import os
+
+GITHUB_USERNAME = os.environ.get("GITHUB_ACTOR")
+FULL_GITHUB_REPOSITORY = os.environ.get("GITHUB_REPOSITORY")
+GITHUB_REPOSITORY = FULL_GITHUB_REPOSITORY.split("/")[1]
+DATA_JSON_FILE_PATH = "_data/data.json"
+README_FILE_PATH = "README.md"
+
+
+def add_help_file(title: str, directory: str, filename: str, category: str):
+    data.append(
+        {
+            "title": title,
+            "url": f"https://{GITHUB_USERNAME}.github.io/{GITHUB_REPOSITORY}/{directory}/{filename}",
+            "category": category,
+        }
+    )
+
+
+def parse_markdown(path: str):
+    with open(path) as md:
+        metadata = frontmatter.load(md).metadata
+
+    return metadata
+
+
+def remove_preexisting_data():
+    if os.path.exists(os.path.abspath("_data/data.json")):
+        os.remove(os.path.abspath("_data/data.json"))
+
+if __name__ == "__main__":
+    data = []
+
+    remove_preexisting_data()
+
+    directory_list = [
+        directory
+        for directory in next(os.walk("."))[1]
+        if directory not in ("images", "Images", "Changelogs")
+        and not directory.startswith("_")
+        and not directory.startswith(".")
+    ]
+
+    for directory in directory_list:
+        for file in os.listdir(os.path.abspath(directory)):
+            help_file_frontmatter = parse_markdown(
+                os.path.abspath(directory + "/" + file)
+            )
+            add_help_file(
+                help_file_frontmatter["title"],
+                directory,
+                file.replace(".md", ""),
+                help_file_frontmatter["category"],
+            )
+
+    if not os.path.isdir(os.path.abspath("_data")):
+        os.mkdir("_data")
+
+    with open(DATA_JSON_FILE_PATH, "w") as data_json:
+        data_json.write(json.dumps(data, indent=4))
+
+    with open(README_FILE_PATH, "r") as readme_file:
+        readme = jinja2.Template(readme_file.read(), trim_blocks=True)
+
+    rendered_readme = readme.render(datasource_url=f"https://github.com/{FULL_GITHUB_REPOSITORY}".replace("//github.com/", "//raw.githubusercontent.com/").replace("/blob/", "/"))
+    readme_output = codecs.open(README_FILE_PATH, "w", "utf-8")
+    readme_output.write(rendered_readme)
+    readme_output.close()
