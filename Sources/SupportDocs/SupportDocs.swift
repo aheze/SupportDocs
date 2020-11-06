2dd29ef7447fab8d4e51df4d59fb31888add4766
Zheng <ahzzheng@gmail.com>
2020-11-05 17:41:39 -0800

    Add test function for SPM

--- Sources/SupportDocs/SupportDocs.swift
+++ Sources/SupportDocs/SupportDocs.swift
@@ -1,3 +1,12 @@
+//struct SupportDocs {
+//    var text = "Hello, World!"
+//}
+
 struct SupportDocs {
     var text = "Hello, World!"
+}
+
+
+public func myTestSPMFunction() {
+    print("SPM Test Function Succeeded!")
 }
