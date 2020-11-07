ad12858bf4d9a09d1393e2d44dd741a82dc67fe2
H. Kamran <hkamran@unisontech.org>
2020-11-07 01:44:56 -0800

    Test TOC functionality

--- _scripts/README.md
+++ _scripts/README.md
@@ -1,8 +1,11 @@
 # SupportDocs: DataSource
-## This is where SupportDocs gets its data!
+This is where SupportDocs gets its data!
 
 ## Data Source URL
 <a id="datasource_url" href="{{ datasource_url }}">{{ datasource_url }}</a>
+
+## Table of Contents
+{{ table_of_contents }}
 
 ## Notes
 Do **not** update this file (`README.md`) directly. Instead, update the file in `_scripts/README.md`.
--- _scripts/datasource_generator.py
+++ _scripts/datasource_generator.py
@@ -76,8 +76,12 @@ if __name__ == "__main__":
     with open(READ_README_FILE_PATH, "r") as readme_file:
         readme = jinja2.Template(readme_file.read(), trim_blocks=True)
 
+    toc = ""
+    for support_document in data:
+        toc += f"- [{support_document['title']}]({support_document['url']})\n"
+
     datasource_url = f"https://github.com/{FULL_GITHUB_REPOSITORY}".replace("//github.com/", "//raw.githubusercontent.com/").replace("/blob/", "/") + f"/{GITHUB_BRANCH}/{DATA_JSON_FILE_PATH}"
-    rendered_readme = readme.render(datasource_url=datasource_url)
+    rendered_readme = readme.render(datasource_url=datasource_url, table_of_contents=toc)
     readme_output = codecs.open(WRITE_README_FILE_PATH, "w", "utf-8")
     readme_output.write(rendered_readme)
     readme_output.close()
