2a9906a4d3ab7191cde8ba5307486a77969ee0ca
Zheng <ahzzheng@gmail.com>
2020-11-20 17:33:40 -0800

    Update screenshots

--- Example/Example/UIKitExampleController.swift
+++ Example/Example/UIKitExampleController.swift
@@ -40,32 +40,28 @@ class UIKitExampleController: UIViewController {
     @IBOutlet weak var presentButton: UIButton!
     @IBAction func presentButtonPressed(_ sender: Any) {
         
-//        let dataSource = URL(string: "https://raw.githubusercontent.com/aheze/SupportDocs/DataSource/_data/supportdocs_datasource.json")!
-//
-//        // MARK: - UIKit way to make `SupportOptions`
-//        var options = SupportOptions()
-//        options.categories = [SupportOptions.Category(tag: "boba", displayName: "Boba is awesome!")]
-//        options.navigationBar.title = "Support"
-//        options.navigationBar.titleColor = UIColor.white
-//        options.navigationBar.dismissButtonView = AnyView(Text("Done"))
-//        options.navigationBar.buttonTintColor = UIColor.white
-//        options.navigationBar.backgroundColor = UIColor(red: 6 / 255, green: 151 / 255, blue: 0 / 255, alpha: 1)
-//        options.progressBar.foregroundColor = UIColor.green
-//        options.progressBar.backgroundColor = UIColor.systemBackground
-//        options.listStyle = .insetGroupedListStyle
-//        options.navigationViewStyle = .defaultNavigationViewStyle
-//        options.other.activityIndicatorStyle = .large
-//        options.other.welcomeView = AnyView(WelcomeView())
-//        options.other.footer = AnyView(Footer())
-//        options.other.error404 = URL(string: "https://aheze.github.io/SupportDocs/404")!
-//
-//        let supportDocsViewController = SupportDocsViewController(dataSourceURL: dataSource, options: options)
-//        self.present(supportDocsViewController, animated: true, completion: nil)
-        
         let dataSource = URL(string: "https://raw.githubusercontent.com/aheze/SupportDocs/DataSource/_data/supportdocs_datasource.json")!
-        
-        let supportDocsViewController = SupportDocsViewController(dataSourceURL: dataSource)
+
+        // MARK: - UIKit way to make `SupportOptions`
+        var options = SupportOptions()
+        options.categories = [SupportOptions.Category(tag: "boba", displayName: "Boba is awesome!")]
+        options.navigationBar.title = "Support"
+        options.navigationBar.titleColor = UIColor.white
+        options.navigationBar.dismissButtonView = AnyView(Text("Done"))
+        options.navigationBar.buttonTintColor = UIColor.white
+        options.navigationBar.backgroundColor = UIColor(red: 6 / 255, green: 151 / 255, blue: 0 / 255, alpha: 1)
+        options.progressBar.foregroundColor = UIColor.green
+        options.progressBar.backgroundColor = UIColor.systemBackground
+        options.listStyle = .insetGroupedListStyle
+        options.navigationViewStyle = .defaultNavigationViewStyle
+        options.other.activityIndicatorStyle = .large
+        options.other.welcomeView = AnyView(WelcomeView())
+        options.other.footer = AnyView(Footer())
+        options.other.error404 = URL(string: "https://aheze.github.io/SupportDocs/404")!
+
+        let supportDocsViewController = SupportDocsViewController(dataSourceURL: dataSource, options: options)
         self.present(supportDocsViewController, animated: true, completion: nil)
+        
     }
 }
 
